fa8d0bbec7a2408add529629c45f675295d8909d
officialdavidtaylor <officialdavidtaylor@gmail.com>
2024-05-29 10:24:30 -0700

    adds prisma and configures Supabase with initial schema

---
+++ prisma/migrations/20240603231433_init/migration.sql
@@ -0,0 +1,118 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "authSource" TEXT NOT NULL,
+    "authId" UUID NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "userId" UUID NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "type" TEXT,
+    "accentColor" TEXT,
+    "dateFirstAvailable" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Merchant" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "title" VARCHAR(255) NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "Merchant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "userId" UUID NOT NULL,
+    "accountId" UUID,
+    "merchantId" UUID NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "description" TEXT,
+    "transactedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "userId" UUID NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "description" TEXT,
+    "accentColor" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "metaCategoryId" UUID,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TransactionByCategory" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "userId" UUID NOT NULL,
+    "transactionId" UUID NOT NULL,
+    "categoryId" UUID NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "TransactionByCategory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MetaCategory" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "userId" UUID NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "description" TEXT,
+    "accentColor" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "MetaCategory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Account_userId_idx" ON "Account"("userId");
+
+-- CreateIndex
+CREATE INDEX "Transaction_userId_idx" ON "Transaction"("userId");
+
+-- CreateIndex
+CREATE INDEX "Transaction_accountId_idx" ON "Transaction"("accountId");
+
+-- CreateIndex
+CREATE INDEX "Transaction_merchantId_idx" ON "Transaction"("merchantId");
+
+-- CreateIndex
+CREATE INDEX "Category_userId_idx" ON "Category"("userId");
+
+-- CreateIndex
+CREATE INDEX "Category_metaCategoryId_idx" ON "Category"("metaCategoryId");
+
+-- CreateIndex
+CREATE INDEX "TransactionByCategory_userId_idx" ON "TransactionByCategory"("userId");
+
+-- CreateIndex
+CREATE INDEX "TransactionByCategory_transactionId_idx" ON "TransactionByCategory"("transactionId");
+
+-- CreateIndex
+CREATE INDEX "TransactionByCategory_categoryId_idx" ON "TransactionByCategory"("categoryId");
+
+-- CreateIndex
+CREATE INDEX "MetaCategory_userId_idx" ON "MetaCategory"("userId");
---
+++ prisma/seed.ts
@@ -0,0 +1,127 @@
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const seedData = [];
+
+  for (let i = 0; i < 10; i += 1) {
+    seedData.push({
+      id: faker.string.uuid(),
+      // assign a known auth uuid to the first generated user
+      authId:
+        i === 0 && process.env.SUPABASE_DEMO_USER_SSID?.length
+          ? process.env.SUPABASE_DEMO_USER_SSID
+          : faker.string.uuid(),
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      accounts: [] as { id: string; title: string }[],
+      categories: [] as { id: string; title: string }[],
+      merchants: [] as { id: string; title: string }[],
+      transactions: [] as {
+        amount: number;
+        transactedAt: Date;
+        accountId: string;
+        merchantId: string;
+      }[],
+    });
+  }
+
+  // append a set of merchants, accounts, and transactions to the seed data
+  for (const profile of seedData) {
+    for (let i = 0; i < 10; i += 1) {
+      profile.accounts.push({
+        id: faker.string.uuid(),
+        title: faker.finance.accountName(),
+      });
+      profile.categories.push({
+        id: faker.string.uuid(),
+        title: faker.commerce.department(),
+      });
+      profile.merchants.push({
+        id: faker.string.uuid(),
+        title: faker.company.name(),
+      });
+    }
+
+    for (let i = 0; i < 100; i += 1) {
+      profile.transactions.push({
+        amount:
+          i % 10 === 0
+            ? faker.number.int({ min: 45, max: 2600 })
+            : faker.number.int({ min: -2000, max: -0.5 }),
+        transactedAt: faker.date.between({
+          from: '2024-01-01T00:00:00.000Z',
+          to: '2024-05-31T00:00:00.000Z',
+        }),
+        accountId: profile.accounts[Math.floor(Math.random() * 10)].id,
+        merchantId: profile.merchants[Math.floor(Math.random() * 10)].id,
+      });
+    }
+  }
+
+  for (const user of seedData) {
+    await prisma.user.create({
+      data: {
+        id: user.id,
+        authId: user.authId,
+        authSource: 'Seed',
+        email: user.email,
+        name: user.name,
+      },
+    });
+
+    for (const account of user.accounts) {
+      await prisma.account.create({
+        data: {
+          id: account.id,
+          title: account.title,
+          userId: user.id,
+        },
+      });
+    }
+
+    for (const category of user.categories) {
+      await prisma.category.create({
+        data: {
+          id: category.id,
+          title: category.title,
+          userId: user.id,
+        },
+      });
+    }
+
+    for (const merchant of user.merchants) {
+      await prisma.merchant.create({
+        data: {
+          id: merchant.id,
+          title: merchant.title,
+        },
+      });
+    }
+
+    for (const transaction of user.transactions) {
+      await prisma.transaction.create({
+        data: {
+          id: faker.string.uuid(),
+          amount: transaction.amount,
+          merchantId: transaction.merchantId,
+          accountId: transaction.accountId,
+          transactedAt: transaction.transactedAt,
+          userId: user.id,
+        },
+      });
+    }
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
